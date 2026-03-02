098a1298c578e6ffd4581801524df7461bab1c08
Simon Downes <simon@simondownes.co.uk>
2026-03-02 23:29:09 +0000

    Sandbox v2

--- sandbox/entrypoint.sh
+++ sandbox/entrypoint.sh
@@ -2,6 +2,15 @@
 
 kiro-cli login --use-device-flow 2>&1 | grep -v "Already logged in" || true
 
-cd "/opt/$SESSION_NAME" || exit 1
-
-exec kiro-cli "$@"
+# Prompt to resume chat if no arguments provided
+if [ $# -eq 0 ]; then
+    read -p "Resume previous chat? [Y/n] " -n 1 -r
+    echo
+    if [[ $REPLY =~ ^[Nn]$ ]]; then
+        exec kiro-cli chat
+    else
+        exec kiro-cli chat --resume
+    fi
+else
+    exec kiro-cli "$@"
+fi
